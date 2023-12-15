ddcbfa5b3942f2917945874aaf566a8f3188550f
Joel Pita Vidal <jpitavidal@danielcastelao.org>
2023-12-15 08:42:44 +0100

    primer commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //imaginemos que aqui hay codigo
 
         Datos dato1 = Datos.getInstance();
         Datos dato2 = Datos.getInstance();
